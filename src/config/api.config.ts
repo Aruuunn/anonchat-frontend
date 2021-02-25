1514d1bbee579d7cb944bccadcd4c587d0aff817
Arun Murugan <arunmurugan200@gmail.com>
2021-02-25 18:06:04 +0530

    Improve User Experience of Smaller Breakpoints by making position of  Bottom Bar fixed

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,7 @@
 import {environment} from '../environments/environment';
 
 
-const DEVELOPMENT_URL = 'http://localhost:8000';
+const DEVELOPMENT_URL = 'http://localhost:8000/api';
 const PRODUCTION_URL = '/api';
 
 export const API_BASE_URL = environment.production ? PRODUCTION_URL : DEVELOPMENT_URL;
