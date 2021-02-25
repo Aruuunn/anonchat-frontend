40e63c8c8a81464bd366ea3c7ab7ec34b44c735e
Arun Murugan <arunmurugan200@gmail.com>
2021-02-25 14:53:54 +0530

    Update api config

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,7 +2,7 @@ import {environment} from '../environments/environment';
 
 
 const DEVELOPMENT_URL = 'http://localhost:8000';
-const PRODUCTION_URL = 'https://anon-chat-web.herokuapp.com';
+const PRODUCTION_URL = '/api';
 
 export const API_BASE_URL = environment.production ? PRODUCTION_URL : DEVELOPMENT_URL;
 export const WEBSOCKET_URI = API_BASE_URL;
